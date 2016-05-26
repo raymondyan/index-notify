af2427bde62fc70e83635410b6bef6c48cb06b54
Jiayang Yan <raymond.dream@gmail.com>
2016-05-26 21:56:01 +0800

    修改了提醒方式

--- value.py
+++ value.py
@@ -5,18 +5,20 @@ import urllib2
 from os import system
 import sys
 from pync import Notifier
+from time import sleep
 
-def tongzhi(num,soup):
+def notification(num,soup):
 	tag = soup(id=num)[0]
 	price = tag.parent.parent(class_="gr")[0].text
 	rate = tag.parent.parent(class_="bg")[0].text
-	brand = tag.parent.parent.a.text
+	brand = tag.parent.parent.a.text.encode('utf-8')
 	value = '%s\t%s' % (price, rate)
-	Notifier.notify(value.encode('utf-8'),subtitle=brand.encode('utf-8'), title='基金估值',  sound='Submarine')
+	Notifier.notify(value.encode('utf-8'),title=brand,group=num, remove=num, sound='Glass')
 	
 if __name__ == "__main__":
 	url = "http://fund.eastmoney.com/fundguzhi.html"
 	content = urllib2.urlopen(url).read()
 	soup = BeautifulSoup(content,"html.parser")
 	for num in sys.argv[1:]:
-		tongzhi(num,soup)
+		notification(num,soup)
+		sleep(5)
